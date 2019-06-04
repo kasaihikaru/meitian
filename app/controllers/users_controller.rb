e322dd14a7855b66b53b33c3f918e5d823815ffc
笠井　光 <01018603@CA2606.local>
2019-06-05 01:41:55 +0900

    status bug fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
     get_user_all_prs
     get_user_recent_prs
     get_user_psr_counts
+    get_user_working_prs
+    get_user_review_needed_prs
   end
 
   def passages
