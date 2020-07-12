19cda143e9b2848fe953b2855448013cbec82c33
Hikaru Kasai <fkrrkf@gmail.com>
2020-07-12 21:09:44 +0900

    exploreの詳細ページのgetロジック修正

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -36,25 +36,25 @@ class ExploresController < ApplicationController
 
 	def passages
 		if user_signed_in?
-			@passages = Passage.active.not_mine(current_user.id).not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@passages = Passage.active.order_mofified_at.page(page_params).per(20)
 		else
-			@passages = Passage.active.not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@passages = Passage.active.order_mofified_at.page(page_params).per(20)
 		end
 	end
 
 	def papers
 		if user_signed_in?
-			@papers = Paper.active.not_mine(current_user.id).not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@papers = Paper.active.order_mofified_at.page(page_params).per(20)
 		else
-			@papers = Paper.active.not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@papers = Paper.active.order_mofified_at.page(page_params).per(20)
 		end
 	end
 
 	def rings
 		if user_signed_in?
-			@rings = Ring.active.not_mine(current_user.id).not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@rings = Ring.active.order_mofified_at.page(page_params).per(20)
 		else
-			@rings = Ring.active.not_sapmle.not_copied.order_mofified_at.page(page_params).per(20)
+			@rings = Ring.active.order_mofified_at.page(page_params).per(20)
 		end
 	end
 end
