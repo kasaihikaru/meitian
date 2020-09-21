b1f51e5d76a76c824a61346e4aa74aafb0c2648b
Hikaru Kasai <fkrrkf@gmail.com>
2020-09-21 19:02:15 +0900

    h1タイトルのフォントを小さく、サンプルデータの上にサンプルであることを示す見出しを消去、ステータスと日中切り替えのマージン狭くする、explores showでの取得ロジックをリファクタ

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -1,29 +1,8 @@
 class ExploresController < ApplicationController
 	def show
-		if user_signed_in?
-			users_all = User.active
-			passages_all = Passage.active.not_sapmle
-			papers_all = Paper.active.not_sapmle
-			rings_all = Ring.active.not_sapmle
-			sentences_all = Sentence.active.not_sapmle
-		else
-			users_all = User.active
-			passages_all = Passage.active.not_sapmle
-			papers_all = Paper.active.not_sapmle
-			rings_all = Ring.active.not_sapmle
-			sentences_all = Sentence.active.not_sapmle
-		end
-		@users = users_all.recent
-		@passages = passages_all.recent
-		@papers = papers_all.recent
-		@rings = rings_all.recent
-		@sentences = sentences_all.recent
-
-		@users_cnt = users_all.count
-		@passages_cnt = passages_all.count
-		@papers_cnt = papers_all.count
-		@rings_cnt = rings_all.count
-		@sentences_cnt = sentences_all.recent
+		@passages = Passage.active.not_sapmle.recent
+		@papers = Paper.active.not_sapmle.recent
+		@rings = Ring.active.not_sapmle.recent
 	end
 
 	def users
