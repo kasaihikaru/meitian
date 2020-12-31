ef702332d9d831e2063ec4a06334a03533290e45
Hikaru Kasai <fkrrkf@gmail.com>
2020-12-31 15:28:03 +0900

    スタンドアロンお試し3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -293,15 +293,14 @@ $(document).on('turbolinks:load',function() {
 
 
   $(function(){
-    var aTags = $('a'); //ページ内のaタグ群を取得。aTagsに配列として代入。
-      //全てのaタグについて処理
-      aTags.each(function(){
-        var url = $(this).attr('href');//aタグのhref属性からリンク先url取得
-        $(this).removeAttr('href');//念のため、href属性は削除
-        $(this).click(function(){
-          location.href = url;//クリックイベントをバインド
-        });
+    var anchor = $('a[target="_blank"]'); 
+    anchor.each(function(){
+      var url = $(this).attr('href');
+      $(this).removeAttr('href');
+      $(this).click(function(){
+        location.href = url;
       });
+    });
   });
 
 });
